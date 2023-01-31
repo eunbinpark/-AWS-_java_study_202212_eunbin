5046f538ed734fc9622dc307196be1d348aac689
eunbinpark <534311@gmail.com>
2023-01-31 17:48:55 +0900

    복

---
+++ src/Test2/복.java
@@ -0,0 +1,17 @@
+package Test2;
+
+public class 복 {
+
+	public static void main(String[] args) {
+		System.out.println("    *       *					");
+		System.out.println("    *       *					");
+		System.out.println("    *********        **	  **	");
+		System.out.println("    *       *       *   *   *	");
+		System.out.println("    *********         *	  *		");
+		System.out.println("        *               *		");
+		System.out.println("  *************					");
+		System.out.println("    *********					");
+		System.out.println("            *					");
+		System.out.println("            *					");
+	}
+}
